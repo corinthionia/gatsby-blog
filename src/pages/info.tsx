961535e676707f5aceed2e4c9f046c30a7c58651
Joohyun Kim <corinthionia@gmail.com>
2022-07-14 16:16:11 +0900

    :sparkles: feat: practice emotion library

--- src/pages/info.tsx
+++ src/pages/info.tsx
@@ -1,5 +1,7 @@
 import React, { FunctionComponent } from 'react'
 import { graphql } from 'gatsby'
+import styled from '@emotion/styled'
+import { Global, css } from '@emotion/react'
 
 interface IInfoPageProps {
   data: {
@@ -13,6 +15,32 @@ interface IInfoPageProps {
   }
 }
 
+/* practicing emotion library */
+
+// global style
+const globalStyle = css`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+
+    font-size: 20px;
+  }
+`
+// styled component (kebab-case)
+const Text1 = styled.div<{ disable: boolean }>`
+  font-size: 20px;
+  font-weight: 700;
+  text-decoration: ${({ disable }) => (disable ? 'line-through' : 'none')};
+`
+
+// object (camelCase)
+const Text2 = styled('div')<{ disable: boolean }>(({ disable }) => ({
+  fontSize: '15px',
+  color: 'blue',
+  textDecoration: disable ? 'line-through' : 'none',
+}))
+
 const InfoPage: FunctionComponent<IInfoPageProps> = function ({
   data: {
     site: {
@@ -22,7 +50,11 @@ const InfoPage: FunctionComponent<IInfoPageProps> = function ({
 }) {
   return (
     <div>
-      {title} {description} {author}
+      <Global styles={globalStyle} />
+      <Text1 disable={true}>{title}</Text1>
+      <Text2 disable={false}>
+        {description} {author}
+      </Text2>
     </div>
   )
 }
